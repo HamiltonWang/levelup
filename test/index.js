4f59fcf87e666e74dc9fb61c9e2de7def97d4c92
Vincent Weevers <mail@vincentweevers.nl>
2018-05-13 15:06:14 +0200

    use plain require in test suite

--- test/index.js
+++ test/index.js
@@ -2,9 +2,23 @@
  * See list at <https://github.com/level/levelup#contributing>
  * MIT License <https://github.com/level/levelup/blob/master/LICENSE.md>
  */
-const fs = require('fs')
-const path = require('path')
 
-fs.readdirSync(__dirname)
-  .filter(f => /-test.js/.test(f))
-  .forEach(f => require(path.join(__dirname, f)))
+// TODO: some tests (like browserify-test) should be skipped in the browser.
+
+require('./argument-checking-test')
+require('./batch-test')
+require('./binary-test')
+require('./browserify-test')
+require('./deferred-open-test')
+require('./get-put-del-test')
+require('./idempotent-test')
+require('./init-test')
+require('./inject-encoding-test')
+require('./json-test')
+require('./key-value-streams-test')
+require('./maybe-error-test')
+require('./no-encoding-test')
+require('./null-and-undefined-test')
+require('./open-patchsafe-test')
+require('./read-stream-test')
+require('./snapshot-test')
